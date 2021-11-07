6fa0cef1fc8a9b778394f8ab31ee83411d615dba
Miliko11 <93069578+Miliko11@users.noreply.github.com>
2021-11-07 11:01:25 +0500

    Add files via upload

---
+++ Калькулятор 7.py
@@ -0,0 +1,26 @@
+
+prodolzit = "Y"
+
+while prodolzit == "Y":
+
+    valueX = float(input("Ведите первое число: "))
+    valueY = float(input("Ведите второе число: "))
+
+    operation = input("Ведите знак операции которую хотите произвести с числами(+, -, *, /): ")
+
+
+    if operation == "+":
+        result = valueX = valueY
+        print("Результат: ", result)
+    elif operation == "-":
+        result = valueX - valueY
+        print("Результат: ", result)
+    elif operation == "*":
+        result = valueX * valueY
+        print("Результат: ", result)
+    elif operation == "/":
+        result = valueX / valueY
+        print("Результат: ", result)
+        if valueY == 0:
+            print("0")
+    prodolzit = input()
